c0e3064d06d9ed1adeea1274567bf439e0e67cb1
Mark <m.godlevskij.987@gmail.com>
2025-05-17 17:21:25 +0300

    Update SecondAvtivityArtifacts.java

--- SecondAvtivityArtifacts.java
+++ SecondAvtivityArtifacts.java
@@ -25,15 +25,16 @@ public class SecondActivityArtifacts extends AppCompatActivity {
     ArrayList<Artifacts> artifacts = new ArrayList<Artifacts>();
     private ArtifactsAdapter adapter;
     private void setInitialData() {
-        artifacts.add(new Artifacts("Горящая алая ведьма", R.drawable.alaya_vedma));
-        artifacts.add(new Artifacts("Возлюбленная юная дева", R.drawable.vozlub_deva));
-        artifacts.add(new Artifacts("Громогласный рёв ярости", R.drawable.grom_rev));
-        artifacts.add(new Artifacts("Изумрудная тень", R.drawable.izumr_ten));
-        artifacts.add(new Artifacts("Сердце глубин", R.drawable.serdce_glyb));
-        artifacts.add(new Artifacts("Конец гладиатора", R.drawable.konec_glad));
-        artifacts.add(new Artifacts("Церемония древней знати", R.drawable.cerem_znati));
-        artifacts.add(new Artifacts("Странствующий ансамбль", R.drawable.stranstvansambl));
-        artifacts.add(new Artifacts("Заблудший в метели", R.drawable.zablydmetel));
+artifacts.add(new Artifacts("Палаюча алая відьма", R.drawable.alaya_vedma));
+artifacts.add(new Artifacts("Кохана юна дівчина", R.drawable.vozlub_deva));
+artifacts.add(new Artifacts("Громовий рев люті", R.drawable.grom_rev));
+artifacts.add(new Artifacts("Ізумрудна тінь", R.drawable.izumr_ten));
+artifacts.add(new Artifacts("Серце глибин", R.drawable.serdce_glyb));
+artifacts.add(new Artifacts("Кінець гладіатора", R.drawable.konec_glad));
+artifacts.add(new Artifacts("Церемонія древньої знаті", R.drawable.cerem_znati));
+artifacts.add(new Artifacts("Мандрівний ансамбль", R.drawable.stranstvansambl));
+artifacts.add(new Artifacts("Загублений у заметілі", R.drawable.zablydmetel));
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,52 +66,54 @@ public class SecondActivityArtifacts extends AppCompatActivity {
         ArtifactsAdapter.OnStateClickListener stateClickListener = new ArtifactsAdapter.OnStateClickListener() {
             @Override
             public void onStateClick(Artifacts state, int position) {
-                switch (state.getName()) {
-                    case "Горящая алая ведьма":
-                        intentDet.putExtra("art", "Горящая Алая Ведьма");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Возлюбленная юная дева":
-                        intentDet.putExtra("art", "Возлюбленная юная дева");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Громогласный рёв ярости":
-                        intentDet.putExtra("art", "Громогласный рёв ярости");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Изумрудная тень":
-                        intentDet.putExtra("art", "Изумрудная тень");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Сердце глубин":
-                        intentDet.putExtra("art", "Сердце глубин");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Конец гладиатора":
-                        intentDet.putExtra("art", "Конец гладиатора");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Церемония древней знати":
-                        intentDet.putExtra("art", "Церемония древней знати");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Странствующий ансамбль":
-                        intentDet.putExtra("art", "Странствующий ансамбль");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
-                    case "Заблудший в метели":
-                        intentDet.putExtra("art", "Заблудший в метели");
-                        boo = 1;
-                        startActivity(intentDet);
-                        break;
+              switch (state.getName()) {
+    case "Палаюча алая відьма":
+        intentDet.putExtra("art", "Палаюча Алая Відьма");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Кохана юна дівчина":
+        intentDet.putExtra("art", "Кохана юна дівчина");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Громовий рев люті":
+        intentDet.putExtra("art", "Громовий рев люті");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Ізумрудна тінь":
+        intentDet.putExtra("art", "Ізумрудна тінь");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Серце глибин":
+        intentDet.putExtra("art", "Серце глибин");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Кінець гладіатора":
+        intentDet.putExtra("art", "Кінець гладіатора");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Церемонія древньої знаті":
+        intentDet.putExtra("art", "Церемонія древньої знаті");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Мандрівний ансамбль":
+        intentDet.putExtra("art", "Мандрівний ансамбль");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+    case "Загублений у заметілі":
+        intentDet.putExtra("art", "Загублений у заметілі");
+        boo = 1;
+        startActivity(intentDet);
+        break;
+}
+
                 }
             }
         };
